7caba4eb8a1ec6b6cdab19987d42160f02b561ce
Haider Ali <rmha64@gmail.com>
2024-06-20 16:34:16 +0500

    Update react-tradingview-widget.jsx

--- src/frontend/Components/react-tradingview-widget.jsx
+++ src/frontend/Components/react-tradingview-widget.jsx
@@ -1,36 +1,36 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 
 export const BarStyles = {
-  BARS: '0',
-  CANDLES: '1',
-  HOLLOW_CANDLES: '9',
-  HEIKIN_ASHI: '8',
-  LINE: '2',
-  AREA: '3',
-  RENKO: '4',
-  LINE_BREAK: '7',
-  KAGI: '5',
-  POINT_AND_FIGURE: '6'
+  BARS: "0",
+  CANDLES: "1",
+  HOLLOW_CANDLES: "9",
+  HEIKIN_ASHI: "8",
+  LINE: "2",
+  AREA: "3",
+  RENKO: "4",
+  LINE_BREAK: "7",
+  KAGI: "5",
+  POINT_AND_FIGURE: "6",
 };
 
 export const IntervalTypes = {
-  D: 'D',
-  W: 'W'
+  D: "D",
+  W: "W",
 };
 
 export const RangeTypes = {
-  YTD: 'ytd',
-  ALL: 'all'
+  YTD: "ytd",
+  ALL: "all",
 };
 
 export const Themes = {
-  LIGHT: 'Light',
-  DARK: 'Dark'
+  LIGHT: "Light",
+  DARK: "Dark",
 };
 
-const SCRIPT_ID = 'tradingview-widget-script';
-const CONTAINER_ID = 'tradingview-widget';
+const SCRIPT_ID = "tradingview-widget-script";
+const CONTAINER_ID = "tradingview-widget";
 
 export default class TradingViewWidget extends PureComponent {
   static propTypes = {
@@ -54,16 +54,16 @@ export default class TradingViewWidget extends PureComponent {
       60,
       120,
       180,
-      '1',
-      '3',
-      '5',
-      '15',
-      '30',
-      '60',
-      '120',
-      '180',
+      "1",
+      "3",
+      "5",
+      "15",
+      "30",
+      "60",
+      "120",
+      "180",
       IntervalTypes.D,
-      IntervalTypes.W
+      IntervalTypes.W,
     ]),
     locale: PropTypes.string,
     news: PropTypes.arrayOf(PropTypes.string),
@@ -71,15 +71,15 @@ export default class TradingViewWidget extends PureComponent {
     popup_height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     popup_width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     range: PropTypes.oneOf([
-      '1d',
-      '5d',
-      '1m',
-      '3m',
-      '6m',
+      "1d",
+      "5d",
+      "1m",
+      "3m",
+      "6m",
       RangeTypes.YTD,
-      '12m',
-      '60m',
-      RangeTypes.ALL
+      "12m",
+      "60m",
+      RangeTypes.ALL,
     ]),
     referral_id: PropTypes.string,
     save_image: PropTypes.bool,
@@ -95,7 +95,7 @@ export default class TradingViewWidget extends PureComponent {
       BarStyles.RENKO,
       BarStyles.LINE_BREAK,
       BarStyles.KAGI,
-      BarStyles.POINT_AND_FIGURE
+      BarStyles.POINT_AND_FIGURE,
     ]),
     symbol: PropTypes.string.isRequired,
     theme: PropTypes.oneOf([Themes.LIGHT, Themes.DARK]),
@@ -104,7 +104,7 @@ export default class TradingViewWidget extends PureComponent {
     watchlist: PropTypes.arrayOf(PropTypes.string),
     widgetType: PropTypes.string,
     width: PropTypes.number,
-    withdateranges: PropTypes.bool
+    withdateranges: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -117,16 +117,16 @@ export default class TradingViewWidget extends PureComponent {
     hide_side_toolbar: true,
     hide_top_toolbar: false,
     interval: IntervalTypes.D,
-    locale: 'en',
+    locale: "en",
     save_image: true,
     show_popup_button: false,
     style: BarStyles.CANDLES,
     theme: Themes.LIGHT,
-    timezone: 'Etc/UTC',
-    toolbar_bg: '#F1F3F6',
-    widgetType: 'widget',
+    timezone: "Etc/UTC",
+    toolbar_bg: "#F1F3F6",
+    widgetType: "widget",
     width: 980,
-    withdateranges: false
+    withdateranges: false,
   };
 
   containerId = `${CONTAINER_ID}-${Math.random()}`;
@@ -138,11 +138,12 @@ export default class TradingViewWidget extends PureComponent {
     this.initWidget();
   };
 
-  canUseDOM = () => !!(
-    typeof window !== 'undefined' &&
-    window.document &&
-    window.document.createElement
-  );
+  canUseDOM = () =>
+    !!(
+      typeof window !== "undefined" &&
+      window.document &&
+      window.document.createElement
+    );
 
   appendScript = (onload) => {
     if (!this.canUseDOM()) {
@@ -152,40 +153,42 @@ export default class TradingViewWidget extends PureComponent {
 
     if (this.scriptExists()) {
       /* global TradingView */
-      if (typeof TradingView === 'undefined') {
+      if (typeof TradingView === "undefined") {
         this.updateOnloadListener(onload);
         return;
       }
       onload();
       return;
     }
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.id = SCRIPT_ID;
-    script.type = 'text/javascript';
+    script.type = "text/javascript";
     script.async = true;
-    script.src = 'https://s3.tradingview.com/tv.js';
+    script.src = "https://s3.tradingview.com/tv.js";
     script.onload = onload;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
   };
 
-  getScriptElement = () =>
-    document.getElementById(SCRIPT_ID);
+  getScriptElement = () => document.getElementById(SCRIPT_ID);
 
-  scriptExists = () =>
-    this.getScriptElement() !== null;
+  scriptExists = () => this.getScriptElement() !== null;
 
   updateOnloadListener = (onload) => {
     const script = this.getScriptElement();
     const oldOnload = script.onload;
-    return script.onload = () => {
+    return (script.onload = () => {
       oldOnload();
       onload();
-    };
+    });
   };
 
   initWidget = () => {
     /* global TradingView */
-    if (typeof TradingView === 'undefined' || !document.getElementById(this.containerId)) return;
+    if (
+      typeof TradingView === "undefined" ||
+      !document.getElementById(this.containerId)
+    )
+      return;
 
     const { widgetType, ...widgetConfig } = this.props;
     const config = { ...widgetConfig, container_id: this.containerId };
@@ -195,15 +198,15 @@ export default class TradingViewWidget extends PureComponent {
       delete config.height;
     }
 
-    if (typeof config.interval === 'number') {
+    if (typeof config.interval === "number") {
       config.interval = config.interval.toString();
     }
 
-    if (config.popup_width && typeof config.popup_width === 'number') {
+    if (config.popup_width && typeof config.popup_width === "number") {
       config.popup_width = config.popup_width.toString();
     }
 
-    if (config.popup_height && typeof config.popup_height === 'number') {
+    if (config.popup_height && typeof config.popup_height === "number") {
       config.popup_height = config.popup_height.toString();
     }
 
@@ -213,16 +216,16 @@ export default class TradingViewWidget extends PureComponent {
 
   cleanWidget = () => {
     if (!this.canUseDOM()) return;
-    document.getElementById(this.containerId).innerHTML = '';
+    document.getElementById(this.containerId).innerHTML = "";
   };
 
   getStyle = () => {
     if (!this.props.autosize) return {};
     return {
-      width: '100%',
-      height: '100%'
+      width: "100%",
+      height: "100%",
     };
   };
 
-  render = () => <article id={this.containerId} style={this.getStyle()} />
+  render = () => <article id={this.containerId} style={this.getStyle()} />;
 }

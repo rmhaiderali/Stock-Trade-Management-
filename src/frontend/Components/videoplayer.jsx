76996a353025de71d556388c155e31dc960b6009
Haider Ali <rmha64@gmail.com>
2024-06-20 16:37:57 +0500

    Update videoplayer.jsx

--- src/frontend/Components/videoplayer.jsx
+++ src/frontend/Components/videoplayer.jsx
@@ -5,16 +5,13 @@ import TradingViewWidget, { Themes } from "./react-tradingview-widget";
 
 const VideoPlayer = ({ selectedStock }) => {
   return (
-    <div className="">
-      <div className="h-[400px] rounded-2xl">
-        {/* Replace video player with TradingView chart */}
-        <TradingViewWidget
-          symbol={selectedStock} // Dynamic symbol based on selected stock
-          theme={Themes.DARK} // Adjust theme as needed
-          locale="en" // Adjust locale as needed
-          autosize
-        />
-      </div>
+    <div className="h-[400px] rounded-2xl overflow-hidden">
+      <TradingViewWidget
+        symbol={selectedStock} // Dynamic symbol based on selected stock
+        theme={Themes.DARK} // Adjust theme as needed
+        locale="en" // Adjust locale as needed
+        autosize
+      />
     </div>
   );
 };

fc58061d5a86eef96473ab3e309cc36dd12d4630
Haider Ali <rmha64@gmail.com>
2024-07-19 10:35:32 +0500

    Serve index.html if no route matches

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,7 +2,9 @@ import "dotenv/config.js";
 import express from "express";
 import app from "./app.js";
 
-app.use(express.static("dist"));
+app.use(express.static("dist"), (req, res) => {
+  res.sendFile("dist/index.html", { root: process.cwd() });
+});
 
 const PORT = process.env.PORT || 3000;
 

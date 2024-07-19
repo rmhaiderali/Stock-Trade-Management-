918a0cf54e91379798f54c33d27d0b5a48bfe6bb
Haider Ali <rmha64@gmail.com>
2024-07-19 13:46:29 +0500

    Add pm2 scripts and ssl server

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,6 +6,40 @@ app.use(express.static("dist"), (req, res) => {
   res.sendFile("dist/index.html", { root: process.cwd() });
 });
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("server started: http://localhost:" + PORT));
+const ssl_key_path = process.env.SSL_KEY_PATH || "ssl/key.pem";
+const ssl_cert_path = process.env.SSL_CERT_PATH || "ssl/cert.pem";
+
+if (
+  fs.existsSync(ssl_key_path) &&
+  fs.existsSync(ssl_cert_path) &&
+  port === "443"
+) {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(ssl_key_path, "utf8"),
+        cert: fs.readFileSync(ssl_cert_path, "utf8"),
+      },
+      app
+    )
+    .listen(443, () =>
+      console.log("Server is started: https://localhost:443")
+    );
+
+  http
+    .createServer((req, res) => {
+      res.writeHead(301, {
+        Location: "https://" + req.headers["host"] + (req.url || "/"),
+      });
+      res.end();
+    })
+    .listen(80, () => {
+      console.log("Redirect server is started: http://localhost:80");
+    });
+} else {
+  app.listen(port, () =>
+    console.log("Server is started: http://localhost:" + port)
+  );
+}
